307a06d35b496b8c06087fb52c2b7e242ec26aec
sarachafikidrissi <chafikidrissisara@gmail.com>
2025-07-29 12:59:47 +0100

    remove button blog from book view

--- resources/views/Book/index.blade.php
+++ resources/views/Book/index.blade.php
@@ -6,17 +6,6 @@
         <h1 class="text-3xl font-semibold text-[#17313E]">Create your book easily </h1>
 
         <button
-            class="blogModalBtn flex items-center text-indigo-700 border border-indigo-600 py-2 px-6 gap-2 rounded inline-flex ">
-            <span>
-                Add blog
-            </span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg"
-                viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                    d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
-            </svg>
-        </button>
-        <button
             class="bookModalBtn flex items-center text-indigo-700 border border-indigo-600 py-2 px-6 gap-2 rounded inline-flex ">
             <span>
                 Add book
